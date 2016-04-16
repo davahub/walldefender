4bacd47cd134c18d73912544405691c5e4845940
dava <davkamail@gmail.com>
2016-04-16 19:15:39 +0100

    initial commit add project files

---
+++ desktop/src/com/mnsoft/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mnsoft.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mnsoft.game.WallDefenderGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WallDefenderGame(), config);
+	}
+}
